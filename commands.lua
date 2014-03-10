30357107c1c295f2e1da8c2b2bcbce0b4bf82787
tonibm19 <tonibm19@gmail.com>
2014-03-10 17:27:11 +0100

    Added /lagg info and /lagg unloadchunks

--- commands.lua
+++ commands.lua
@@ -1,6 +1,6 @@
 function HandleLaggClearCommand(Split, Player)
     if Split[2] == nil then
-        Player:SendMessage("Usage: /lagg [clear/check/killmobs]")
+        Player:SendMessage("Usage: /lagg [clear/check/killmobs/info/unloadchunks]")
         return true
     elseif Split[2] == "clear" then
         Time = TimeUntilRemove - 1
@@ -37,6 +37,50 @@ function HandleLaggClearCommand(Split, Player)
         cRoot:Get():ForEachWorld(ForEachWorld)
         Player:SendMessage("Killed "..Mobs.." mobs")
         Mobs = 0
+        return true
+    elseif Split[2] == "info" then
+        Root = cRoot:Get()
+        ramusage = cRoot:GetPhysicalRAMUsage() / 1024
+        swapusage = cRoot:GetVirtualRAMUsage() / 1204
+        totalusage = ramusage + swapusage
+        Player:SendMessage("Physical RAM use: "..ramusage.." mb")
+        Player:SendMessage("Virtual RAM use: "..swapusage.." mb")
+        Player:SendMessage("Total RAM use: "..totalusage.." mb")
+        Player:SendMessage("Current loaded chunks: "..Root:GetTotalChunkCount())
+        return true
+    elseif Split[2] == "unloadchunks" then
+        World = Player:GetWorld()
+        Root = cRoot:Get()
+        playername = Player:GetName()
+        ramusage = cRoot:GetPhysicalRAMUsage() / 1024
+        swapusage = cRoot:GetVirtualRAMUsage() / 1204
+        totalusage = ramusage + swapusage
+        local Out1 = "Num loaded chunks before: " .. cRoot:Get():GetTotalChunkCount()
+        local Out2 = "Ram usage before: " .. totalusage
+        Root:SaveAllChunks()
+        local UnloadChunks = function(World)
+            World:QueueUnloadUnusedChunks()
+        end
+        local QuequeLoopWorlds = function(World)
+            cRoot:Get():ForEachWorld(UnloadChunks)
+        end
+        local TellPlayer = function(Player)
+            afterramusage = cRoot:GetPhysicalRAMUsage() / 1024
+            afterswapusage = cRoot:GetVirtualRAMUsage() / 1024
+            aftertotalusage = afterramusage + afterswapusage
+            Out3 = "Num loaded chunks after: " .. cRoot:Get():GetTotalChunkCount()
+            Out4 = "Ram usage after: " .. aftertotalusage
+            Player:SendMessage(Out1)
+            Player:SendMessage(Out2)
+            Player:SendMessage(Out3)
+            Player:SendMessage(Out4)
+        end
+        local LoopTellPlayer = function(Player)
+            World:FindAndDoWithPlayer(playername, TellPlayer)
+        end
+        World:ScheduleTask(100, QuequeLoopWorlds)
+        World:ScheduleTask(200, LoopTellPlayer)
+        Player:SendMessage("Unloading... Please wait a moment")
         return true
     end
 end
