d0f0f3111a496634d28fd2fd7e7714cf88a64482
Mathias <mathias-github@users.noreply.github.com>
2015-07-18 00:55:15 +0300

    Changed description

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ function Initialize(Plugin)
 	Plugin:SetName("ClearLagg")
 	Plugin:SetVersion(0)
 	
-	cPluginManager.BindCommand( "/lagg",          "clearlagg.clear",      HandleLaggClearCommand,   " ClearLagg" )
+	cPluginManager.BindCommand( "/lagg",          "clearlagg.clear",      HandleLaggClearCommand,   "- Remove entities and unload chunks." )
 
 	cPluginManager.AddHook(cPluginManager.HOOK_TICK, OnTick)
 	cPluginManager.AddHook(cPluginManager.HOOK_SPAWNING_MONSTER, OnSpawningMonster)
